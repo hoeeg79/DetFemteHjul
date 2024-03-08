16f6810e838bc0288b5a5ffdb3726762e3d8f444
Lasse Høeg <93433669+hoeeg79@users.noreply.github.com>
2024-03-08 09:58:38 +0100

    New frontend that works/start of the main page

--- frontendV2/src/app/app-routing.module.ts
+++ frontendV2/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomepageComponent} from "./homepage/homepage.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: HomepageComponent
+    component: HomePageComponent
   }
 ];
 
--- frontendV2/src/app/app.module.ts
+++ frontendV2/src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomePageComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
