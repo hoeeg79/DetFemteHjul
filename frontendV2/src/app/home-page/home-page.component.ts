1e10cd18a5b3392190dc40cd5efd62d562711881
ShadySloth <pedant01@easv365.dk>
2024-03-13 14:24:33 +0100

    jeg skubber til lasse

--- frontendV2/src/app/home-page/home-page.component.ts
+++ frontendV2/src/app/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StateService } from '../state.service';
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
@@ -8,6 +9,7 @@ import { StateService } from '../state.service';
 })
 export class HomePageComponent {
   toTranslate?: string;
+  chosenLanguage?: string;
   fromLanguage?: string;
   constructor(public state: StateService) {}
 
@@ -15,9 +17,19 @@ export class HomePageComponent {
     if (!this.fromLanguage){
       this.fromLanguage = "NoLanguageSelected";
     }
+    let languageCode;
+    console.log(this.chosenLanguage);
+    for (let i = 0; i < this.state.languages!.length; i++) {
+      console.log(this.state.languages![i]);
+      if (this.state.languages![i] === this.chosenLanguage){
+        languageCode = this.state.code![i];
+        break
+      }
+    }
     var dto = {
       eventType: "ClientWantsToTranslate",
       messageToTranslate: this.toTranslate,
+      chosenLanguage: languageCode,
       fromLanguage: this.fromLanguage,
     }
     this.state.ws.send(JSON.stringify(dto));
